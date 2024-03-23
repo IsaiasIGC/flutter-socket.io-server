70abd75d74a7121b60187dd9b4d5de52ba5c1f66
Isaias Gregori Crespo <isaiasgrecre@gmail.com>
2024-03-23 20:33:37 +0100

    Cambios sobre bandas terminado

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,10 +1,20 @@
 const { io } = require('../index');
+const Bands = require('../models/bands');
+const Band = require('../models/band');
+
+const bands = new Bands();
+
+bands.addBand( new Band( 'Guns & Roses' ) );
+bands.addBand( new Band( 'Bon Jovi' ) );
+bands.addBand( new Band( 'Queen' ) );
+bands.addBand( new Band( 'Metallica' ) );
 
 
 // Mensajes de Sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
 
+    client.emit('active-bands', bands.getBands() );
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
@@ -16,5 +26,31 @@ io.on('connection', client => {
         io.emit( 'mensaje', { admin: 'Nuevo mensaje' } );
 
     });
+
+    client.on('vote-band', ( payload ) => {
+
+        bands.voteBand( payload.id );
+        io.emit('active-bands', bands.getBands() );
+
+    });
+
+    client.on('add-band', ( payload ) => {
+        const newBand = new Band( payload.name );
+        bands.addBand( newBand );
+        io.emit('active-bands', bands.getBands() );
+
+    });
+
+    client.on('delete-band', ( payload ) => {
+
+        bands.deleteBand( payload.id );
+        io.emit('active-bands', bands.getBands() );
+
+    });
+    // client.on('emitir-mensaje', ( payload ) => {
+        // console.log(payload);
+        // io.emit('nuevo-mensaje', payload ); // emite a todos!
+        // client.broadcast.emit('nuevo-mensaje', payload ); // emite a todos menos al emisor
+    // });
 
 });
